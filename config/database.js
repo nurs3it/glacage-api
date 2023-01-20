dfbbd4b8e3bdd59e1b749d680031e029b8310f10
Nurseiit Sharip <nurseit.js@gmail.com>
2023-01-21 02:25:26 +0600

    fix: fix db

--- config/database.js
+++ config/database.js
@@ -1,10 +1,13 @@
-const path = require('path');
-
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'postgres',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env('PGHOST', '127.0.0.1'),
+      port: env.int('PGPORT', 6802),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'password'),
+      ssl: env.bool(true),
     },
     useNullAsDefault: true,
   },
